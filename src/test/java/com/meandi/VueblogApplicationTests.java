e511b084d309b40f603e8f00da1a9180a9cc6840
rubbish123 <2358357084@qq.com>
2020-12-22 17:02:22 +0800

    临时用了下

--- src/test/java/com/meandi/VueblogApplicationTests.java
+++ src/test/java/com/meandi/VueblogApplicationTests.java
@@ -2,12 +2,18 @@ package com.meandi;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import cn.hutool.crypto.SecureUtil;
 
 @SpringBootTest
 class VueblogApplicationTests {
 
     @Test
     void contextLoads() {
+    }
+
+    @Test
+    public void test(){
+        System.out.println(SecureUtil.md5("831143"));
     }
 
 }
